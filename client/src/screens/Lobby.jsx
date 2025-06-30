302e32ba94c4a84bc5130e7d537e58818900e9ca
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-06-30 15:26:46 +0530

    feat(client): create LobbyScreen component for the client application

---
+++ client/src/screens/Lobby.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LobbyScreen = () => {
+    return(
+        <div className="lobby-screen">
+            <h1>Lobby</h1>
+        </div>
+    )
+}
+
+export default LobbyScreen;
