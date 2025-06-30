b6fb3688099640224a4a3e14990c43e94f88160b
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-06-30 15:50:34 +0530

    feat(client): implement email and room input form in LobbyScreen component

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState , useCallback} from "react";
+import { use } from "react";
 
 const LobbyScreen = () => {
-    return(
-        <div className="lobby-screen">
-            <h1>Lobby</h1>
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [room, setRoom] = useState("");
+
+  const handleSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    console.log({
+        email,
+        room,
+    });
+  },[email, room])
+  return (
+    <div>
+      <h1>Lobby</h1>
+      <form onSubmit={handleSubmitForm}>
+        <label htmlFor="email"> Email ID</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Enter your email"
+          required
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <label htmlFor="room"> Room Number</label>
+        <input type="text" id="room" placeholder="Enter your Room" required value={room}
+          onChange={(e) => {
+            setRoom(e.target.value);
+          }}/>
+        <br />
+        <br />
+        <button type="submit">Join Room</button>
+      </form>
+    </div>
+  );
+};
 
 export default LobbyScreen;
